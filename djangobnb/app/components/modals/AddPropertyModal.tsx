83efd97c06992f72f75ad1445c17ce71a49faf68
Nithin1729S <sureshnithin1729@gmail.com>
2024-12-16 05:53:38 +0530

    add multi-step functionality to AddPropertyModal

--- djangobnb/app/components/modals/AddPropertyModal.tsx
+++ djangobnb/app/components/modals/AddPropertyModal.tsx
@@ -1,19 +1,40 @@
 "use client";
 import Image from "next/image";
 import Modal from "./Modal";
+import { useState } from "react";
 
 import useAddPropertyModal from "@/app/hooks/useAddPropertyModal";
 import LoginModal from "./LoginModal";
+import CustomButton from "../forms/CustomButton";
 
 const AddPropertyModal = () => {
+  const [currentStep, setCurrentStep] = useState(1);
   const addPropertyModal = useAddPropertyModal();
+  const content=(
+    <>
+    {currentStep==1?(
+        <>
+          <h2 className="mb-6 text-2xl">Choose Category</h2>
+          <CustomButton
+            label="Next"
+            onClick={()=>setCurrentStep(2)}
+      />
+        </>
+    ): (
+      <p>Step 2</p>
+    )
+
+    }
+      
+    </>
+  )
   return (
     <>
       <Modal
         isOpen={addPropertyModal.isOpen}
         close={addPropertyModal.close}
         label="Add Property"
-        content={<p>hi</p>}
+        content={content}
       ></Modal>
     </>
   );
